f8893515b28ff3cacb4794d7c3c381fe3f9b98a9
Yasmin Iveth Vargas Godoy <yasmin.vargas@laureate.net>
2020-12-17 21:07:40 -0600

    updated

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 const express = require("express");
+const categories = require("./routes/categories.routes");
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("Welcome");
+    res.send("Welcome!");
 });
 
-app.get("/categories", (req, res) => {
-    res.send("All categories");
-});
+app.use(categories);
 
 app.listen(port, ()=> {
     console.log(`Server is running on port ${port}`);
